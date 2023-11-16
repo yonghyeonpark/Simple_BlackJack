b5beb1f06c3058a5c3e529b39cf858cd31e674e0
YongHyeon <pyh4616@naver.com>
2023-11-17 04:11:13 +0900

    feat(Score): 점수 설정에 무승부를 추가하고, 승리 패배 무승부 각 상황마다 횟수를 추가하는 기능 구현

--- src/main/java/blackjack/model/Score.java
+++ src/main/java/blackjack/model/Score.java
@@ -15,5 +15,18 @@ public class Score {
     private void setUp() {
         score.put("승", 0);
         score.put("패", 0);
+        score.put("무", 0);
+    }
+
+    public void addVictory() {
+        score.put("승", score.get("승") + 1);
+    }
+
+    public void addDefeat() {
+        score.put("패", score.get("패") + 1);
+    }
+
+    public void addDraw() {
+        score.put("무", score.get("무") + 1);
     }
 }
