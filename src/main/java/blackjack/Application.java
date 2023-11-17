d9be6aa22b1e0deed860222a6de7f1a4eb3a3a08
YongHyeon <pyh4616@naver.com>
2023-11-18 00:05:04 +0900

    feat(Application): 프로그램 실행 기능 구현

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -1,7 +1,22 @@
 package blackjack;
 
+import blackjack.controller.Controller;
+import blackjack.model.Dealer;
+import blackjack.model.Player;
+import blackjack.service.BlackjackGame;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+import java.io.IOException;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Player player = new Player();
+        Dealer dealer = new Dealer();
+        BlackjackGame blackJackGame = new BlackjackGame(player, dealer);
+        Controller controller = new Controller(inputView, outputView, blackJackGame);
+        controller.play();
     }
 }
