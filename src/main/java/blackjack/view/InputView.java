27043afd9b1dc61d44239a702329673ba7b1dffc
YongHyeon <pyh4616@naver.com>
2023-11-17 21:50:49 +0900

    feat(InputView): 게임 추가 진행 여부를 입력하는 기능 구현

---
+++ src/main/java/blackjack/view/InputView.java
@@ -0,0 +1,22 @@
+package blackjack.view;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputView {
+
+    public String readGameDecision() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String gameDecision = reader.readLine();
+        validate(gameDecision);
+        return gameDecision;
+    }
+
+    private void validate(String inputGameDecision) {
+        String gameDecision = inputGameDecision.toUpperCase();
+        if (!gameDecision.equals("Y") && !gameDecision.equals("N")) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
