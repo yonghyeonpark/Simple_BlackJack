f933c45ce9ffe227cdda5c05c97fb26d0f35f593
YongHyeon <pyh4616@naver.com>
2023-11-17 16:32:54 +0900

    refactor(BlackjackGame): 생성자에 Dealer 다시 추가, 게임 라운드 변수 추가, compareCardNumber 메서드 안에서 번호를 생성하도록 변경 및 게임 라운드 증감 추가

--- src/main/java/blackjack/service/BlackjackGame.java
+++ src/main/java/blackjack/service/BlackjackGame.java
@@ -1,21 +1,29 @@
 package blackjack.service;
 
+import blackjack.model.Dealer;
 import blackjack.model.Player;
 
 public class BlackjackGame {
 
     private final Player player;
+    private final Dealer dealer;
+    private int gameRound;
 
-    public BlackjackGame(Player player) {
+    public BlackjackGame(Player player, Dealer dealer) {
         this.player = player;
+        this.dealer = dealer;
+        this.gameRound = 1;
     }
 
-    public String compareCardNumber(int playerCardNumber, int dealerCardNumber) {
-        if (playerCardNumber > dealerCardNumber) {
+    public String compareCardNumber() {
+        int playerCurrentCardNumber = player.getCurrentCardNumber(gameRound);
+        int dealerCurrentCardNumber = dealer.getCurrentCardNumber(gameRound);
+        gameRound++;
+        if (playerCurrentCardNumber > dealerCurrentCardNumber) {
             player.win();
             return "당신이 이겼습니다.";
         }
-        if (playerCardNumber < dealerCardNumber) {
+        if (playerCurrentCardNumber < dealerCurrentCardNumber) {
             player.defeated();
             return "딜러가 이겼습니다.";
         }
