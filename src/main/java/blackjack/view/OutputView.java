a3d2e8119eec05467ca1222868992c3ccd2c9842
YongHyeon <pyh4616@naver.com>
2023-11-17 23:59:19 +0900

    feat(OutputView): 게임 종료 메시지를 출력하는 기능 구현

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -11,7 +11,12 @@ public class OutputView {
         System.out.println(information);
     }
 
-    public void printIsGameContinue() {
+    public void printIsGameContinueMessage() {
         System.out.print("한 게임 더 하시겠습니까? (Y / N) ");
+    }
+
+    public void printExitMessage() {
+        System.out.println("게임을 종료합니다.");
+        System.out.println("플레이해주셔서 감사합니다.");
     }
 }
