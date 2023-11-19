89d3927cf3c9a5ee3a5257367c57121502f4292a
YongHyeon <pyh4616@naver.com>
2023-11-19 23:20:52 +0900

    feat(Property): 배팅 금액에 대한 유효성 검사 기능 구현

--- src/main/java/blackjack/model/Property.java
+++ src/main/java/blackjack/model/Property.java
@@ -9,10 +9,21 @@ public class Property {
     }
 
     public void bet(int bettingAmount) {
+        validateRange(bettingAmount);
+        validateUnit(bettingAmount);
+        property -= bettingAmount;
+    }
+
+    private void validateRange(int bettingAmount) {
         if (property - bettingAmount < 0) {
             throw new IllegalArgumentException("잘못 입력하셨습니다.");
         }
-        property -= bettingAmount;
+    }
+
+    private void validateUnit(int bettingAmount) {
+        if (bettingAmount % 100 != 0) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다.");
+        }
     }
 
     public void plusBettingAmount(int bettingAmount) {
