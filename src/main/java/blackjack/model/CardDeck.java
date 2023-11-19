cd7faa59ded3e780949cb29d75c9f46d0ed72590
YongHyeon <pyh4616@naver.com>
2023-11-20 02:46:42 +0900

    refactor(CardDeck): 카드 덱을 처음부터 생성하도록 수정

--- src/main/java/blackjack/model/CardDeck.java
+++ src/main/java/blackjack/model/CardDeck.java
@@ -1,42 +1,44 @@
 package blackjack.model;
 
 import blackjack.util.CardNumberGenerator;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class CardDeck {
 
-    private final Map<Integer, Integer> cardDeck;
+    private final Queue<Integer> cardDeck;
 
     public CardDeck() {
-        this.cardDeck = setUp();
+        this.cardDeck = new LinkedList(setUp());
     }
 
-    private Map<Integer, Integer> setUp() {
-        Map<Integer, Integer> cardDeck = new HashMap<>();
-        for (int i = 1; i < 11; i++) {
-            cardDeck.put(i, 4);
+    public List<Integer> setUp() {
+        List<Integer> cardDeck = new ArrayList<>();
+        CardNumberGenerator cardNumberGenerator = new CardNumberGenerator();
+        for (int i = 1; i <= 10; i++) {
+            repeatFour(i);
         }
-        cardDeck.put(11, 12);
+        for (int i = 0; i < 12; i++) {
+            cardDeck.add(11);
+        }
+        Collections.shuffle(cardDeck);
         return cardDeck;
     }
 
-    public int pullCard() {
-        CardNumberGenerator cardNumberGenerator = new CardNumberGenerator();
-        while (true) {
-            int cardNumber = cardNumberGenerator.generateCardNumber();
-            if (cardDeck.get(cardNumber) >= 1) {
-                cardDeck.put(cardNumber, cardDeck.get(cardNumber) - 1);
-                return cardNumber;
-            }
+    private void repeatFour(int cardNumber) {
+        for (int i = 0; i < 4; i++) {
+            cardDeck.add(cardNumber);
         }
     }
 
+    public int pullCard() {
+        return cardDeck.poll();
+    }
+
     public int countCardDeck() {
-        int count = 0;
-        for (Map.Entry<Integer, Integer> entry : cardDeck.entrySet()) {
-            count += entry.getValue();
-        }
-        return count;
+        return cardDeck.size();
     }
 }
