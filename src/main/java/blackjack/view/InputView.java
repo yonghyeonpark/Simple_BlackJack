ea94bcb4fe448e21b0c08893cd468cefe9c715e4
YongHyeon <pyh4616@naver.com>
2023-11-20 01:45:29 +0900

    feat(InputView): 게임 진행 혹은 카드 뽑기 결정을 입력하는 기능 구현

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -7,8 +7,26 @@ import java.io.InputStreamReader;
 
 public class InputView {
 
+    private static final String YES = "Y";
+    private static final String NO = "N";
+    private static final String ERROR_MESSAGE = "잘못 입력하셨습니다.";
+
     public int readBettingAmount() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return Convert.stringToInteger(reader.readLine());
+    }
+
+    public String readGameOrCardDecision() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String gameOrCardDecision = reader.readLine().toUpperCase();
+        validate(gameOrCardDecision);
+        System.out.println();
+        return gameOrCardDecision;
+    }
+
+    private void validate(String gameOrCardDecision) {
+        if (!gameOrCardDecision.equals(YES) && !gameOrCardDecision.equals(NO)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
     }
 }
