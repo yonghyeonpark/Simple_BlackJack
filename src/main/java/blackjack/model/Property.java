70cc0b38fc8125e72a5b0254e505d692afa26649
YongHyeon <pyh4616@naver.com>
2023-11-19 22:32:45 +0900

    feat(Property): 베팅하는 기능 구현

---
+++ src/main/java/blackjack/model/Property.java
@@ -0,0 +1,17 @@
+package blackjack.model;
+
+public class Property {
+
+    private int property;
+
+    public Property() {
+        this.property = 1000;
+    }
+
+    public void bet(int bettingAmount) {
+        if (property - bettingAmount < 0) {
+            throw new IllegalArgumentException("잘못 입력하셨습니다.");
+        }
+        property -= bettingAmount;
+    }
+}
