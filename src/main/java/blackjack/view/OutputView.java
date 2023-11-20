b5b3877f3be10be3a7fedb0572ec67fd6115d4fc
YongHyeon <pyh4616@naver.com>
2023-11-21 05:34:43 +0900

    feat(OutputView): 게임 결과를 출력하는 기능 구현

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -3,6 +3,7 @@ package blackjack.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "간단 카드 게임을 시작합니다.";
+    private static final String GAME_RESULT_MESSAGE = "플레이 해 주셔서 감사합니다.";
 
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -10,5 +11,10 @@ public class OutputView {
 
     public void printGameProgressInformation(String information) {
         System.out.println(information);
+    }
+
+    public void printGameResult(String gameResult) {
+        System.out.println(gameResult);
+        System.out.println(GAME_RESULT_MESSAGE);
     }
 }
