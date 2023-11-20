d65f1b988fafa3f3cdc14add7c3cad53ae0ac7f0
YongHyeon <pyh4616@naver.com>
2023-11-21 05:23:14 +0900

    feat(OutputView): 게임 시작 메시지 출력 기능 구현

---
+++ src/main/java/blackjack/view/OutputView.java
@@ -0,0 +1,10 @@
+package blackjack.view;
+
+public class OutputView {
+    
+    private static final String GAME_START_MESSAGE = "간단 카드 게임을 시작합니다.";
+
+    public void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
