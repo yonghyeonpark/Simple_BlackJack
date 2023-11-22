9cb29d23f32f772c08e9487f7b2c292d419cdf61
YongHyeon <pyh4616@naver.com>
2023-11-22 15:46:52 +0900

    refactor(Convert): 중복된 예외 처리 삭제

--- src/main/java/blackjack/util/Convert.java
+++ src/main/java/blackjack/util/Convert.java
@@ -3,10 +3,6 @@ package blackjack.util;
 public class Convert {
 
     public static int stringToInteger(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("잘못 입력하셨습니다.");
-        }
+        return Integer.parseInt(input);
     }
 }
